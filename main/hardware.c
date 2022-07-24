b79cfa269166b1b4b3f0690e6ea59dc7eb30b99f
kaitec <8434163@gmail.com>
2022-07-24 18:40:49 +0300

    small changes

--- main/hardware.c
+++ main/hardware.c
@@ -5,10 +5,47 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <driver/gpio.h>
 #include <app_reset.h>
 #include <ws2812_led.h>
 #include "hardware.h"
+
+esp_timer_handle_t fast_timer; //   1 ms
+esp_timer_handle_t slow_timer; // 100 ms
+
+void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    //uint32_t gpio_num = (uint32_t) arg;
+    //if(gpio_num==MOTOR_FB) motor_hall_feedback();
+}
+
+void fast_timer_callback(void *priv) // 1 ms
+{
+    //if(motor.feedback==MOTOR_NO_FEEDBACK)   motor_no_fb_timer_function();
+}
+
+void slow_timer_callback(void *priv) // 100 ms
+{
+
+}
+
+void timer_init(void)
+{
+    esp_timer_create_args_t slow_timer_config = {
+        .callback = slow_timer_callback,
+        .dispatch_method = ESP_TIMER_TASK,
+        .name = "slow"};
+    esp_timer_create(&slow_timer_config, &slow_timer);
+
+    esp_timer_create_args_t fast_timer_config = {
+        .callback = fast_timer_callback,
+        .dispatch_method = ESP_TIMER_TASK,
+        .name = "fast"};
+    esp_timer_create(&fast_timer_config, &fast_timer);
+
+    esp_timer_start_periodic(slow_timer, 100000U); 
+    esp_timer_start_periodic(fast_timer,   1000U); 
+}
 
 void gpio_init(void)
 {
@@ -21,7 +58,8 @@ void gpio_init(void)
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
-    //gpio_set_level(LED_STATUS_R, 1);
+    gpio_set_level(LED_R, 1); gpio_set_level(UP_DIR, 0);
+    gpio_set_level(LED_G, 1); gpio_set_level(DOWN_DIR, 0);
 
     // GPIO IN INIT
     io_conf.intr_type = GPIO_INTR_ANYEDGE;// GPIO_INTR_NEGEDGE // GPIO_INTR_POSEDGE // GPIO_INTR_DISABLE;
@@ -31,39 +69,34 @@ void gpio_init(void)
     gpio_config(&io_conf);
 
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //gpio_isr_handler_add(HALL_IN, gpio_isr_handler, (void*) HALL_IN);
+    gpio_isr_handler_add(MOTOR_FB, gpio_isr_handler, (void*) MOTOR_FB);
+}
+
+void hardware_init()
+{
+    gpio_init();
+    timer_init();
+
+    // ws2812_led_init();
+    // app_reset_button_register(app_reset_button_create(RESET_BUTTON, RESET_ACTIVE_LEVEL),
+    //                            WIFI_RESET_BUTTON_TIMEOUT, FACTORY_RESET_BUTTON_TIMEOUT);
 }
 
 esp_err_t app_fan_set_power(bool power)
 {
-    if (power) 
-    {
-        gpio_set_level(OUTPUT_GPIO, 1);
-    } 
-    else 
-    {
-        gpio_set_level(OUTPUT_GPIO, 0);
-    }
+    // if (power) 
+    // {
+    //     gpio_set_level(OUTPUT_GPIO, 1);
+    // } 
+    // else 
+    // {
+    //     gpio_set_level(OUTPUT_GPIO, 0);
+    // }
     return ESP_OK;
 }
 
 esp_err_t app_fan_set_speed(uint8_t height, uint8_t angle)
 {
-    return ws2812_led_set_rgb(0, height, angle*8);
-}
-
-void hardware_init()
-{
-    ws2812_led_init();
-    app_reset_button_register(app_reset_button_create(RESET_BUTTON, RESET_ACTIVE_LEVEL),
-                               WIFI_RESET_BUTTON_TIMEOUT, FACTORY_RESET_BUTTON_TIMEOUT);
-
-    /* Configure GPIO */
-    gpio_config_t io_conf = {
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = 1,
-    };
-    io_conf.pin_bit_mask = ((uint64_t)1 << OUTPUT_GPIO);
-    /* Configure the GPIO */
-    gpio_config(&io_conf);
+    //return ws2812_led_set_rgb(0, height, angle*8);
+    return ESP_OK;
 }
--- main/hardware.h
+++ main/hardware.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define PCB_REV   1
+#define PCB_REV   D4//1
 
 #if(PCB_REV==1)
 #define RESET_BUTTON        0
@@ -10,6 +10,16 @@
 #define OUTPUT_GPIO         2
 #define GPIO_OUTPUT_PIN_SEL ((1ULL<<OUTPUT_GPIO))
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<RESET_BUTTON))
+#elif(PCB_REV==D4)
+#define LED_R               17
+#define LED_G               16
+#define LED_B               15
+#define MOTOR_FB            14
+#define UP_DIR              13
+#define DOWN_DIR            12
+#define BUTTON               0
+#define GPIO_OUTPUT_PIN_SEL ((1ULL<<LED_R) | (1ULL<<LED_G) | (1ULL<<LED_B) | (1ULL<<UP_DIR) | (1ULL<<DOWN_DIR))
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<MOTOR_FB) | (1ULL<<BUTTON))
 #endif
 
 #define DEBUG  NONE
@@ -22,6 +32,7 @@
 
 
 void gpio_init(void);
+void timer_init(void);
 void hardware_init(void);
 esp_err_t app_fan_set_power(bool power);
 esp_err_t app_fan_set_speed(uint8_t height, uint8_t angle);
